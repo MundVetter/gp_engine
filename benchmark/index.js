46beec4ca7d0e142137f276ab9352ce9de3f6405
Mund Vetter <mundvetter@gmail.com>
2017-10-08 21:02:44 +0200

    added more test and a benchmark

---
+++ benchmark/index.js
@@ -0,0 +1,29 @@
+const Benchmark = require('benchmark')
+const Game = require('../index.js')
+const microtime = require('microtime')
+
+const suite = new Benchmark.Suite
+const control = new Game()
+let startTime;
+let endTime;
+
+suite
+  .add('update', () => {
+    control.update([{
+        paddle: control.paddles[0],
+        action: 'up'
+      }, {
+        paddle: control.paddles[1],
+        action: 'down'
+      }
+    ])
+  })
+  .on('start', () => {
+    startTime = microtime.now()
+  })
+  .on('cycle', (event) => {
+    endTime = microtime.now()
+    console.log(`Total elapsed time: ${(endTime - startTime)/ 1000000} seconden.`)
+    console.log(String(event.target))
+  })
+  .run()
--- test/index.js
+++ test/index.js
@@ -34,11 +34,47 @@ describe('Glitchping', () => {
     for (let i = 0; i < 100; i++) {
       bounce.update()
     }
+
     it('should change the direction of the ball', () => {
       assert.equal(bounce.ball.xDirection, 'right')
     })
+
     it('should change the player who controls the ball', () => {
       assert.ok(bounce.paddles[0].controlsBall)
+    })
+  })
+
+  describe('#move', () => {
+    const move = new Game()
+    move.ball.moveY('up')
+    move.paddles[0].move('left')
+
+    it('should inverse the direction given to the ball in the y-axis', () => {
+      assert.equal('down', move.ball.physicsY.powerDirection)
+    })
+
+    it('should set the power of the ball in y-axis to the maxPower', () => {
+      assert.equal(move.ball.maxPower, move.ball.physicsY.power)
+    })
+
+    it('should set the paddle direction to power direction to the ', () => {
+      assert.equal('left', move.paddles[0].physics.powerDirection)
+    })
+
+    it('should set the power of the paddle to the maxPower', () => {
+      assert.equal(move.paddles[0].maxPower, move.paddles[0].physics.power)
+    })
+  });
+
+  describe('#reset', ()=> {
+    const reset = new Game();
+    for (let i = 0; i < 30; i++) {
+      reset.update()
+    }
+    reset.ball.reset()
+
+    it('it should set the the ball back to the start position', () => {
+      assert.deepEqual(reset.ball.startPos, reset.ball.pos)
     })
   })
 
